8bacb801d6986d95e829ae5f413baff33469e983
Benbolf <you@example.com>
2023-10-21 11:46:16 -0500

    Initial Commit

---
+++ src/main/java/recipes/Projects.java
@@ -0,0 +1,14 @@
+package recipes;
+
+import java.sql.Connection;
+
+import recipes.dao.DbConnection;
+
+public class Projects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       DbConnection.getConnection();
+	}
+
+}
